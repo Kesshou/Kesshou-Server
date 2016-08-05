c24bd8efc37a7734da575f616e3f0357b8e2babf
blackkite0206233 <samuel991640@gmail.com>
2016-08-05 18:53:44 +0800

    set

--- routes/scorequery.js
+++ routes/scorequery.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/historyscore', function(req, res, next) {
 });
 
 module.exports = router;
