448c87dde6a67726c58b0c97d6fed24286da78c4
yoyo930021 <yoyo930021@gmail.com>
2016-08-17 16:06:40 +0800

    測試redis 修改timeout module

--- Kesshou/Repositories/RedisReopsitory.js
+++ Kesshou/Repositories/RedisReopsitory.js
@@ -4,39 +4,42 @@
 */
 var redis = require('redis');
 
-var existTime = 60 * 60 * 24 * 30; // 30 days
+var existTime = 60 * 30; // 30 mins
 var cache = redis.createClient();
+
+client.on('ready',function(err){
+    console.log('ready');
+});
 
 cache.on("error", function(err) {
     console.log(err);
 });
 
-/*
-*Author: blackkite0206233
-*Description:
-    This function is used to add a data to redis server.
-*Usage:
-    account: the user's account.
-    token: a sentence contains 20 random characters.
-*/
-function set(account, token) {
-    cache.set(token, account);
-    cache.expire(token, existTime);
-}
+module.exports = {
+        /*
+        *Author: blackkite0206233
+        *Description:
+            This function is used to add a data to redis server.
+        *Usage:
+            account: the user's account.
+            token: a sentence contains 20 random characters.
+        */
+        set : function(token, account) {
+            cache.set(token, account);
+            cache.expire(account, existTime);
+        },
+        /*
+        *Author: blackkite0206233
+        *Description:
+            This function is used to check if the token is valid and get user's account from redis server.
+        *Usage:
+            token: a sentence contains 20 random characters.
+            reply: the user's account, if the token is not valid then the reply is "".
 
-/*
-*Author: blackkite0206233
-*Description:
-    This function is used to check if the token is valid and get user's account from redis server.
-*Usage:
-    token: a sentence contains 20 random characters.
-    reply: the user's account, if the token is not valid then the reply is "".
-
-*/
-function getAccount(token) {
-    cache.get(token, function(err, reply) {
-        return reply;
-    });
-}
-
-module.exports = RedisRepository;
+        */
+        getAccount : function(token) {
+            cache.get(token, function(err, reply) {
+                return reply;
+            });
+        }
+};
--- Kesshou/Repositories/UserRepository.js
+++ Kesshou/Repositories/UserRepository.js
@@ -5,101 +5,103 @@
 
 
 var models  = require('../models');
-/*
-*Author: blackkite0206233,yoyo930021
-*Description:
-    This function is used to get the use's password.
-*Usage:
-    account: user's account(email).
-    return: user's password, if the account doen't exist, return "".
-*/
-function getUserPassword(account) {
-    models.Account.findOne({ where: {email: account} }).then(function(result){
-        return result.get('pwd');
-    }).catch(function(error) {
-        debug(error.toString());
-        return "";
-    });
-};
 
-/*
-*Author: blackkite0206233,yoyo930021
-*Description:
-    This function is used to create a new user.
-*Usage:
-    email: user's email(account).
-    password: user's password.
-    userGroup: user's status(student or graduated or outside).
-    schoolAccount: user's school account(optional).
-    schoolPwd: user's school password(optional).
-    nick: user's nick.
-    return: if create successfully, return true, otherwise return false.
-*/
-function createUser(email, password, userGroup, schoolAccount, schoolPwd, nick,name) {
-    models.Group.findOne({ where: {comment:userGroup} }).then(function(group){
-        var NewAccount={
-            email:email,
-            pwd:password,
-            group_id:group.get("id"),
-            school_account:schoolAccount,
-            school_pwd:schoolPwd,
-            nick:nick,
-            name:name
-        }
-        models.Account.create(NewAccount).then(function(account) {
-            return true;
+module.exports = {
+    /*
+    *Author: blackkite0206233,yoyo930021
+    *Description:
+        This function is used to get the use's password.
+    *Usage:
+        account: user's account(email).
+        return: user's password, if the account doen't exist, return "".
+    */
+    getUserPassword:function(account) {
+        models.Account.findOne({ where: {email: account} }).then(function(result){
+            return result.get('pwd');
         }).catch(function(error) {
             debug(error.toString());
-            return false;
+            return "";
         });
-    }).catch(function(error){
-        debug(error.toString());
-        return false;
-    });
-    
-    
-}
+    },
 
-/*
-*Author: blackkite0206233,yoyo930021
-*Description:
-    This function is used to update user's info.
-*Usage:
-    account: user's old account.
-    newSchoolPwd: user's new school password(optional).
-    newNick: user's new nick.
-    newPassword: user's new password.
-    newEmail: user's new email.
-    return: if update successfully, return true, otherwise return false.
-*/
-function updateUserInfo(account, newSchoolPwd, newNick, newPassword, newEmail) {
-    models.Account.findOne({where:{email:account}}).then(function(result){
-        result.update({school_pwd:newSchoolPwd,nick:newNick,pwd:newPassword,email:newEmail}).then(function(result){
-            return true;
+    /*
+    *Author: blackkite0206233,yoyo930021
+    *Description:
+        This function is used to create a new user.
+    *Usage:
+        email: user's email(account).
+        password: user's password.
+        userGroup: user's status(student or graduated or outside).
+        schoolAccount: user's school account(optional).
+        schoolPwd: user's school password(optional).
+        nick: user's nick.
+        return: if create successfully, return true, otherwise return false.
+    */
+    createUser:function (email, password, userGroup, schoolAccount, schoolPwd, nick,name) {
+        models.Group.findOne({ where: {comment:userGroup} }).then(function(group){
+            var NewAccount={
+                email:email,
+                pwd:password,
+                group_id:group.get("id"),
+                school_account:schoolAccount,
+                school_pwd:schoolPwd,
+                nick:nick,
+                name:name
+            }
+            models.Account.create(NewAccount).then(function(account) {
+                return true;
+            }).catch(function(error) {
+                debug(error.toString());
+                return false;
+            });
         }).catch(function(error){
             debug(error.toString());
             return false;
         });
-    }).catch(function(error){
-        debug(error.toString());
-        return false;
-    });
-}
+        
+        
+    },
 
-/*
-*Author: blackkite0206233,yoyo930021
-*Description:
-    This function is used to get user's information.
-*Usage:
-    account: user's account.
-    return: if update successfully, return a Object, otherwise return null.
-*/
-function getUserInfo(account) {
-    models.Account.findOne({where:{email:account}}).then(function(result){
-        return result;
-    }).catch(function(error){
-        debug(error.toString());
-        return null;
-    });
-}
-module.exports = UserRepository;
+    /*
+    *Author: blackkite0206233,yoyo930021
+    *Description:
+        This function is used to update user's info.
+    *Usage:
+        account: user's old account.
+        newSchoolPwd: user's new school password(optional).
+        newNick: user's new nick.
+        newPassword: user's new password.
+        newEmail: user's new email.
+        return: if update successfully, return true, otherwise return false.
+    */
+    updateUserInfo:function (account, newSchoolPwd, newNick, newPassword, newEmail) {
+        models.Account.findOne({where:{email:account}}).then(function(result){
+            result.update({school_pwd:newSchoolPwd,nick:newNick,pwd:newPassword,email:newEmail}).then(function(result){
+                return true;
+            }).catch(function(error){
+                debug(error.toString());
+                return false;
+            });
+        }).catch(function(error){
+            debug(error.toString());
+            return false;
+        });
+    },
+
+    /*
+    *Author: blackkite0206233,yoyo930021
+    *Description:
+        This function is used to get user's information.
+    *Usage:
+        account: user's account.
+        return: if update successfully, return a Object, otherwise return null.
+    */
+    getUserInfo:function (account) {
+        models.Account.findOne({where:{email:account}}).then(function(result){
+            return result;
+        }).catch(function(error){
+            debug(error.toString());
+            return null;
+        });
+    }
+};
