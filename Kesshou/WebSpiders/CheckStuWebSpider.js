17de52177a899a16de4f2c3f3c9edb7265733064
blackkite0206233 <samuel991640@gmail.com>
2016-08-19 01:26:11 +0800

    create CheckStuWebSpiders

---
+++ Kesshou/WebSpiders/CheckStuWebSpider.js
@@ -0,0 +1,61 @@
+var request=require("request");
+var cheerio=require("cheerio");
+var fs = require("fs");
+var iconv = require('iconv-lite');
+
+module.exports = 
+        /*
+        *Author: yoyo930021
+        *Description:
+            This function is used to check student exist.
+        *Usage:
+            stu_id : student system account.
+            stu_pwd : student system password.
+            callback : then do.
+        */
+        function(stu_id,stu_pwd,callback) {
+
+            var j =request.jar();
+
+            var formLogin={
+                url:"https://stuinfo.taivs.tp.edu.tw/Reg_Stu.ASP",
+                form: {"txtS_NO": stu_id, "txtPerno": stu_pwd},
+                jar:j,
+                agentOptions: {
+                    ca: fs.readFileSync(__dirname+"/cert/taivsca.crt"),
+                },
+                encoding: "binary",
+                followAllRedirects: true
+            };
+
+            var formI={
+                url:"https://stuinfo.taivs.tp.edu.tw/stu_0.ASP",
+                jar:j,
+                agentOptions: {
+                    ca: fs.readFileSync(__dirname+"/cert/taivsca.crt"),
+                },
+                encoding: "binary"
+            };
+
+            request.post(formLogin,function (err,res,body) {
+                if(err){
+                    console.log(err);
+                    callback("")
+                }else{
+                    if(parseInt(res.headers['content-length']) < 2500){
+                        request(formI,function (err,res,body) {
+                            if(err){
+                                console.log(err);
+                                callback("");
+                            }else{
+                                var $ = cheerio.load(iconv.decode(new Buffer(body, "binary"),"Big5"));
+                                callback($("b").eq(4).text().substr(3));
+                            }
+                        });
+                    }else{
+                        callback("");
+                    }
+                }
+            });
+            
+        }
