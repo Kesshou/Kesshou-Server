6841b9c8d73ecbc93600dde101b4c5387a6dfc5c
blackkite0206233 <samuel991640@gmail.com>
2016-08-26 15:37:37 +0800

    create AnnounceRepository.js

---
+++ Kesshou/Repositories/AnnounceRepository.js
@@ -0,0 +1,17 @@
+/*
+*Author: blackkite0206233
+*Description: This file is used to control the announcement data.
+*/
+
+var models  = require('../../models');
+
+module.exports = {
+
+    getAnnouncement:function(type) {
+
+    },
+
+    getCollection:function(user) {
+
+    }
+};
