7ac15836e46ddaaf23e973d63b4721cb19d699af
rice9547 <s5590220001@gmail.com>
2016-08-26 15:14:26 +0800

    add forum

---
+++ routes/forum.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+
+var router = express.Router();
+
+router.get('/students', function (req, res, next) {
+
+})
+
+router.get('/graduate', function (req, res, next) {
+
+})
+
+router.get('/announcement', function (req, res, next) {
+
+})
+
+router.get('/collect', function (req, res, next) {
+
+})
+
+
+
+module.exports = router;
