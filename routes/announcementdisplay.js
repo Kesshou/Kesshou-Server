b11fee642c2e0efe3a525d7428ee023e3b24f63a
blackkite0206233 <samuel991640@gmail.com>
2016-08-26 15:41:49 +0800

    set announce API

--- routes/announcementdisplay.js
+++ routes/announcementdisplay.js
@@ -4,25 +4,36 @@
 */
 var express = require('express');
 var RedisRepository = require('../Kesshou/Repositories/RedisRepository');
+var AnnounceRepository = require('../Kesshou/Repositories/AnnounceRepository');
 var router = express.Router();
 
+/*
+*Author: blackkite0206233
+*Description:
+    This function is the API which was used to return announcement.
+*Usage:
+    return:
+        status code:
+            200: register successfully.
+        title:
+            The announcement's title.
+        content:
+            The announcement's content.
+        date:
+            The announcement's release date.
+*/
 router.get('/announce', function(req, res, next) {
-
-});
-
-router.post('/announce', function(req, res, next) {
-    var newAnnounce = req.body;
-
-});
-
-router.put('/announce', function(req, res, next) {
-    var updateAnnounce = req.body;
-
-});
-
-router.delete('/announce', function(req, res, next) {
-    var deleteAnnounce = req.body;
-
+    var announce;
+    var type = req.type;
+    if(type != "collect") {
+        announce = AnnounceRepository.getAnnouncement(type);
+    } else {
+        var user = RedisRepository.getAccount(req.token);
+        announce = AnnounceRepository.getCollection(user);
+    }
+    res.status(200).json({"title" : announce.title,
+                                        "content" : announce.content,
+                                        "date" : announce.release_date});
 });
 
 module.exports = router;
