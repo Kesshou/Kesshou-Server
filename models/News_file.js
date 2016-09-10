063c9e73538c3934c14e2d7a7a189933343ed9d7
blackkite0206233 <samuel991640@gmail.com>
2016-09-10 13:50:38 +0800

    edit News_file model

--- models/News_file.js
+++ models/News_file.js
@@ -3,7 +3,7 @@ module.exports = function(sequelize, DataTypes) {
     var News_file = sequelize.define('News_file', {
         news_key: DataTypes.INTEGER,
         type: DataTypes.STRING,
-        file_name: DataTypes.INTEGER,
+        file_name: DataTypes.STRING,
         file_src: DataTypes.STRING,
     }, {
         classMethods: {
