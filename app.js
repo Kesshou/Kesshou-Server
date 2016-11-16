3ec04ec960d64e6daa29e16f6ed8a404602cdc4c
yoyo930021 <yoyo930021@gmail.com>
2016-11-16 19:06:50 +0800

    modify static

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static',express.static( __dirname + '/public'));
 
 app.use('/' + version + '/actmanage', actmanage);
 app.use('/' + version + '/announcementdisplay', announcementdisplay);
